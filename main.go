421079e0b92917ded68c9369f8c0b7c5129ef032
Caleb Albers <calbers@hashicorp.com>
2022-12-22 15:36:43 -0800

    Add support for .ejs files

--- main.go
+++ main.go
@@ -349,6 +349,8 @@ func licenseHeader(path string, tmpl *template.Template, data licenseData) ([]by
 		lic, err = executeTemplate(tmpl, data, "", "// ", "")
 	case ".ml", ".mli", ".mll", ".mly":
 		lic, err = executeTemplate(tmpl, data, "(**", "   ", "*)")
+	case ".ejs":
+		lic, err = executeTemplate(tmpl, data, "<%/*", "  ", "*/%>")
 	default:
 		// handle various cmake files
 		if base == "cmakelists.txt" || strings.HasSuffix(base, ".cmake.in") || strings.HasSuffix(base, ".cmake") {
